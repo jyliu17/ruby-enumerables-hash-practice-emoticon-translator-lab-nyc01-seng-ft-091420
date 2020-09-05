a2fd94833af161bd851ddb26e962a8d69e46a64a
Johnson Liu <jyliu17@gmail.com>
2020-09-05 02:33:25 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -7,6 +7,9 @@ def load_library(path)
   # code goes here
   final_hash = {}
   YAML.load_file(path).each do |key, value|
+    final_hash[key] = {}
+    final_hash[key][:english] = value[0]
+    final_hash[key][:japanese] = value[1]
   end
   final_hash
 end
