9331d9b20d410aefd41968540bc3c6a2aeb29561
Johnson Liu <jyliu17@gmail.com>
2020-09-05 02:40:29 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -16,7 +16,11 @@ end
 
 def get_japanese_emoticon(path, emoticon)
   # code goes here
-  load_library(path)
+  load_library(path).each do |key, value|
+    if value[:japanese] == emoticon
+      return key
+    end
+  end
 end
 
 def get_english_meaning
